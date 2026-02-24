6b7b55c10e39d5f1f0506083cff9f57ba037a5c1
Ms dem <demanmosolov78@gmail.com>
2026-02-24 14:42:30 +1000

    Второй коммит(уже с большой буквы и у коммита две m

--- Feature1/feature1.go
+++ Feature1/feature1.go
@@ -2,7 +2,23 @@ package feature1
 
 import "fmt"
 
+type people struct {
+	name   string
+	adress string
+	age    int
+}
+
 func Feature1() {
 
 	fmt.Println("Я первая фича")
+
+	People := people{
+		name:   "ДаНИл колбасенко",
+		adress: "ДОм чикатило",
+		age:    11,
+	}
+	fmt.Println("Инфа о человеке: ")
+	fmt.Println("Имя: ", People.name)
+	fmt.Println("Адресс: ", People.adress)
+	fmt.Println("Возраст: ", People.age)
 }
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	feature1 "study2/Feature1"
+	"time"
 )
 
 func main() {
 
 	fmt.Println("Саламалейкум")
 	feature1.Feature1()
+	for i := 0; i < 5; i++ {
+		fmt.Println("Как же многа  строк")
+		time.Sleep(1 * time.Second)
+	}
 }
